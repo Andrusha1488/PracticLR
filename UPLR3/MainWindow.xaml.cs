720f562d14f0a29f2356963eb6f3ed0972f01a52
Andrey Ilin <isip_an.n.ilyin@mpt.ru>
2024-03-23 15:25:24 +0300

    3 prct

---
+++ UPLR3/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UPLR3.BurgerPlaceDataSetTableAdapters;
+
+namespace UPLR3
+{
+ 
+    public partial class MainWindow : Window
+    {
+        Orders_CustomersTableAdapter orders_customers = new Orders_CustomersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Orders_CustomersDataGrid.ItemsSource = orders_customers.GetData();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+    }
+}
---
+++ UPLR3/Window1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UPLR3
+{
+    public partial class Window1 : Window
+    {
+        private BurgerPlaceEntities1 context = new BurgerPlaceEntities1();
+        public Window1()
+        {
+            InitializeComponent();
+            Orders_CustomersDataGrid.ItemsSource = context.Orders_Customers.ToList();
+        }
+    }
+}
